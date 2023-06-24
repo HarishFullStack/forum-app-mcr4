40118f1bbe285b4fc1963463bb57a6028d2772f9
HarishFullStack <harishgupta.dev@gmail.com>
2023-06-24 22:05:53 +0530

    actions comment, share, bookmark added

--- src/App.js
+++ src/App.js
@@ -10,11 +10,14 @@ function App() {
   const reducer = (state, actions) => {
     switch(actions.type){
       case "LIKE":
-        return {...state, forumData: state.forumData.posts.map((x) => x.upvotes = x.id === actions.value ? ++x.upvotes : x.upvotes)}
+        return {...state, forumData: {...state.forumData, posts: state.forumData.posts.map((x) => ({...x, upvotes: x.postId === actions.value ? x.upvotes + 1 : x.upvotes}))}}
 
       case "DISLIKE":
-        return {...state, forumData: state.forumData.posts.map((x) => x.downvotes = x.id === actions.value ? ++x.downvotes : x.downvotes)}
+        return {...state, forumData: {...state.forumData, posts: state.forumData.posts.map((x) => ({...x, downvotes: x.postId === actions.value ? ++x.downvotes - 1 : x.downvotes}))}}
 
+      case "BOOKMARK":
+        return {...state, forumData: {...state.forumData, posts: state.forumData.posts.map((x) => ({...x, isBookmarked: x.postId === actions.value ? !x.isBookmarked : x.isBookmarked}))}}
+      
       default:
         return state;
     }
@@ -55,25 +58,30 @@ function App() {
       <div className='v-left'></div>
       <div className='w-30 p-0'>
         <div className="main-content">
-          <h4 className="p-2">Latest Posts</h4>
+          <div><h4 className="p-2">Latest Posts</h4></div>
           {state.forumData.posts.map((post) => {
             return(
               <div key={post.postId} className={`d-flex mb-5 post-container`}>
                 <div className="d-flex vote-options align-items-center p-2">
                   <div className="cursor-pointer" onClick={() => dispatch({type: "LIKE", value: post.postId})}><img alt="like" src={require("./images/icons8-like-32.png")}></img></div>
                   <div className="cursor-pointer">{post.upvotes - post.downvotes}</div>
-                  <div className="cursor-pointer" onClick={() => dispatch({type: "DILIKE", value: post.postId})}><img alt="dislike" src={require("./images/icons8-dislike-32.png")}></img></div> </div>
+                  <div className="cursor-pointer" onClick={() => dispatch({type: "DISLIKE", value: post.postId})}><img alt="dislike" src={require("./images/icons8-dislike-32.png")}></img></div> </div>
                     <div><div className="d-flex">
                         <img className="avatar cursor-pointer" alt={post.username} src={post.picUrl}></img><div className="d-flex align-items-center">Posted by <b className="username cursor-pointer">@{post.username}</b> | {dayjs(post.createdAt).format("MMM")} {dayjs(post.createdAt).format("D")}</div>
                     </div>
                     <div className="w-100">
-                        
                         <h5>{post.post}</h5>
+                          {post.tags.map((tag) => {
+                            return(
+                              <span className="badge rounded-pill text-bg-success me-1 mb-1">{tag}</span>
+                            )})}
                         <p>{post.postDescription}</p>
                         <div className="">
-                            <div className="d-flex">
-                                {/* {data.post.likes && <div className="col-md-4"><img className="actions cursor-pointer" src={data.post.likes.likedBy.find((by) => by._id === data.user._id) !== undefined ? require('../../images/heart-red.png') : require('../../images/heart-empty.png')} onClick={() => data.post.likes.likedBy.find((by) => by._id === data.user._id) !== undefined ? data.handleDisLikeClick(data.post._id) : data.handleLikeClick(data.post._id)}></img> {data.post.likes.likeCount}</div>}
-                                {data.post.likes && <div className="col-md-4"><img className="actions cursor-pointer" src={data.bookmarks.find((bookmark) => bookmark._id === data.post._id) !== undefined ? require('../../images/bookmark-green.png') : require('../../images/bookmark-black.png') } onClick={() => data.bookmarks.find((bookmark) => bookmark._id === data.post._id) !== undefined ? data.handleRemoveBookmarkClick(data.post._id) : data.handleBookmarkClick(data.post._id)}></img></div>} */}
+                            <div className="d-flex" style={{width: "90%"}}>
+                                <div className="col-md-4  d-flex justify-content-start"><img className="actions cursor-pointer" alt="comment" src={require('./images/icons8-comment-32.png')}></img></div>
+                                <div className="col-md-4  d-flex justify-content-center"><img className="actions cursor-pointer" alt="share"  src={require('./images/icons8-share-32.png')}></img></div>
+                                <div className="col-md-4 d-flex justify-content-end"><img className="actions cursor-pointer" alt="bookmark"  src={post.isBookmarked ? require('./images/bookmark-green.png') : require('./images/bookmark-black.png')} onClick={() => dispatch({type: "BOOKMARK", value: post.postId})}></img></div>
+
                             </div>
                         </div>
                     </div></div>
